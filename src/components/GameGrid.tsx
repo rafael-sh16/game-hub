9ead7139c93e2aa57fd73f5128aaf7b73231681b
Rafael Henriques <rafael.henriques99@gmail.com>
2023-09-04 14:24:48 +0100

    Highlight the selected genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -27,11 +27,12 @@ const GameGrid = ({ selectedGenre }: Props) => {
               <GameCardSkeleton />
             </GameCardContainer>
           ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
-          </GameCardContainer>
-        ))}
+        {!isLoading &&
+          data.map((game) => (
+            <GameCardContainer key={game.id}>
+              <GameCard game={game} />
+            </GameCardContainer>
+          ))}
       </SimpleGrid>
     </>
   );
